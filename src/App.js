bd15f67fca56f140772e43fbe7bb9229af26cba4
Anushk Jain <anushk2201@gmail.com>
2023-12-13 12:32:28 +0530

    Friendlist Rendering Done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Aryan",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Anshika",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Suyash",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
 ];
 
-
 function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const FriendsList = () => {
+  const friends = initialFriends;
+
+  return(
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+const Friend = ({friend}) => {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && <p className="red">You owe {friend.name} &#8377; {Math.abs(friend.balance)}</p>}
+
+      {friend.balance > 0 && <p className="green">{friend.name} owes you &#8377; {Math.abs(friend.balance)}</p>}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
+}
